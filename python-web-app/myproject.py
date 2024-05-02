5ee56f2dba24bb2a5c2b44f88c0798d84995fa8b
hy <hassan@example.com>
2024-05-02 09:58:39 +0000

    Fix

--- python-web-app/myproject.py
+++ python-web-app/myproject.py
@@ -1,11 +1,18 @@
+import logging
 from flask import Flask
 from flask_cors import CORS
 from redis import Redis, ConnectionError
 from elasticapm.contrib.flask import ElasticAPM
+from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 CORS(app)
-apm = ElasticAPM(app)
-redis = Redis(host="redis.redis-db.svc.cluster.local", port=6379, password="123")
+apm = ElasticAPM(app, logging=True)
+redis = Redis(host="redis.redis-db", port=6379, password="123")
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+logging.basicConfig(filename='record.log',format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s' , level=logging.DEBUG)
+
 
 app.config['ELASTIC_APM'] = {
 'SERVICE_NAME': 'python-flaskk-app',
@@ -14,9 +21,11 @@ app.config['ELASTIC_APM'] = {
 }
 @app.route("/")
 def main():
+    app.logger.info("Log From main function")
     return "<h1 style='color:blue'>main!</h1>"
 @app.route("/second")
 def second():
+    app.logger.error("Log From second function")
     return "<h1 style='color:red'>second</h1>"
 @app.route("/redis")
 def write():
@@ -29,6 +38,12 @@ def health_check():
     else:
         return 'Service Unavailable', 500
 
+app.route('/metrics')
+def metrics():
+    return { 
+            'status' '200'
+            }
+
 def all_required_services_are_running():
     try:
         redis.ping()
@@ -36,5 +51,5 @@ def all_required_services_are_running():
     except ConnectionError:
         return False
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
